59a0cec4b509893d3d2e7617c42fd5f74033e90c
m4ppy <leon.choi.dev@gmail.com>
2026-05-03 11:22:39 +0900

    docs: 6주차 내용 정리

--- Leon/umc10th/src/main/java/com/example/umc10th/domain/member/converter/MemberConverter.java
+++ Leon/umc10th/src/main/java/com/example/umc10th/domain/member/converter/MemberConverter.java
@@ -1,4 +1,40 @@
 package com.example.umc10th.domain.member.converter;
 
+import com.example.umc10th.domain.member.dto.MemberRequestDTO;
+import com.example.umc10th.domain.member.dto.MemberResponseDTO;
+import com.example.umc10th.domain.member.entity.Member;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberConverter {
+
+    public Member toMember(MemberRequestDTO.Signup dto) {
+        return Member.builder()
+                .email(dto.getEmail())
+                .name(dto.getName())
+                .gender(dto.getGender())
+                .birthDate(dto.getBirthDate())
+                .address(dto.getAddress())
+                .socialType("LOCAL")
+                .socialUid(dto.getEmail()) // 임시
+                .point(0)
+                .build();
+    }
+
+    public MemberResponseDTO.GetMyInfo toGetMyInfo(Member member) {
+        return MemberResponseDTO.GetMyInfo.builder()
+                .memberId(member.getId())
+                .email(member.getEmail())
+                .name(member.getName())
+                .point(member.getPoint())
+                .build();
+    }
+
+    public MemberResponseDTO.Login toLogin(Member member) {
+        return MemberResponseDTO.Login.builder()
+                .memberId(member.getId())
+                .email(member.getEmail())
+                .name(member.getName())
+                .build();
+    }
 }
