c26f5090e83224c8595194c80845e162eb546d59
soseongmin03 <sosungmin0122@gmail.com>
2026-05-19 12:19:14 +0900

    docs:Chapter08.md 최종

--- mino/springboot/main/java/org/example/swaggerpr/global/security/CustomUserDetailsService.java
+++ mino/springboot/main/java/org/example/swaggerpr/global/security/CustomUserDetailsService.java
@@ -15,8 +15,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = memberRepository.findByEmail(username)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Member not found."));
 
         return User.withUsername(member.getEmail())
